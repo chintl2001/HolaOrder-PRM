c70d42c71de80b6aa14870ee8d34a6ae935a9276
chilinhnguyen <nguyenlinhchitqp@gmail.com>
2023-06-22 00:48:55 +0700

    edit login

--- HolaOrder/app/src/main/java/com/example/holaorder/SignIn.java
+++ HolaOrder/app/src/main/java/com/example/holaorder/SignIn.java
@@ -16,53 +16,53 @@ import org.jetbrains.annotations.NotNull;
 public class SignIn extends AppCompatActivity {
     EditText edtUsername, edtPassword;
     Button btnSignIn;
-    @SuppressLint("MissingInflatedId")
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
-        edtUsername = (EditText) findViewById(R.id.edtUsername);
-        edtPassword = (EditText) findViewById(R.id.edtPassword);
-        btnSignIn = (Button) findViewById(R.id.btnSignin);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSignIn = findViewById(R.id.btn_signin);
 
-        //Firebase
+        // Firebase
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference table_user = database.getReference("User");
-         btnSignIn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 ProgressDialog mDialog = new ProgressDialog(SignIn.this);
-                 mDialog.setMessage("Please wwaiting ....");
-                 mDialog.show();
-                 table_user.addValueEventListener(new ValueEventListener() {
-                     @Override
-                     public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                         //check user if not exist in database
-                         if(snapshot.child(edtUsername.getText().toString()).exists()){
-                             //Get user information
-                             mDialog.dismiss();
-                             User user = snapshot.child(edtUsername.getText().toString()).getValue(User.class);
-                             if(user.getPassword().equals(edtPassword.getText().toString())){
-                                 Toast.makeText(SignIn.this, "Sign in successfully !", Toast.LENGTH_SHORT).show();
-                             }else{
-                                 Toast.makeText(SignIn.this, "Sign in failed !", Toast.LENGTH_SHORT).show();
-                             }
-                         }
-                         else{
-                             mDialog.dismiss();
-                             Toast.makeText(SignIn.this, "User not exist in database ", Toast.LENGTH_SHORT).show();
-                         }
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please waiting...");
+                mDialog.show();
 
-                     }
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        // Check if user exists in the database
+                        if (snapshot.child(edtUsername.getText().toString()).exists()) {
+                            // Get user information
+                            mDialog.dismiss();
+                            User user = snapshot.child(edtUsername.getText().toString()).getValue(User.class);
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                Toast.makeText(SignIn.this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignIn.this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "User does not exist in the database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-                     @Override
-                     public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-                     }
-                 });
-             }
-         });
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                        // Handle onCancelled event
+                    }
+                });
+            }
+        });
     }
 }
