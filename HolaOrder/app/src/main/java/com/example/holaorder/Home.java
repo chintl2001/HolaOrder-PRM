ce8249c776e609161c1f91dc098ebaac103b8649
huypnd <135795418+huypnd27698@users.noreply.github.com>
2023-06-27 08:46:42 +0700

    Update home

--- HolaOrder/app/src/main/java/com/example/holaorder/Home.java
+++ HolaOrder/app/src/main/java/com/example/holaorder/Home.java
@@ -6,17 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import com.example.holaorder.Adapter.CategoryAdapter;
 import com.example.holaorder.Adapter.PopularAdapter;
+
+import com.example.holaorder.Common.Common;
+
 import com.example.holaorder.Domain.CategoryDomain;
 import com.example.holaorder.Domain.FoodDomain;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
-
+    TextView textItem;
     private RecyclerView.Adapter adapter, adapter2 ;
     private RecyclerView recyclerViewCaregoryList, recyclerViewPopularList;
     @Override
@@ -24,8 +34,25 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference table_category = database.getReference("Category");
+        DatabaseReference table_product = database.getReference("Product");
+
+        ((TextView) findViewById(R.id.textHello)).setText("Hello, " + Common.currentUser.getUsername());
         recyclerViewCaregory();
         recyclerViewPopular();
+
+        textItem = ((TextView)findViewById(R.id.textItem));
+
+        textItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent product = new Intent(Home.this,ListProduct.class);
+                startActivity(product);
+            }
+        });
+
+
     }
 
     private void recyclerViewCaregory() {
