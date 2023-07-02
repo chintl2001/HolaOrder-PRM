1b154b31ba17ca5e9bdcd1f8dd074d1d15b3d099
quynhbn272 <115777816+quynhbn27@users.noreply.github.com>
2023-07-02 22:57:56 +0700

    Add remember, Add To Cart, CartView

---
+++ HolaOrder/app/src/main/java/com/example/holaorder/CartActivity.java
@@ -0,0 +1,103 @@
+package com.example.holaorder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.holaorder.Interface.ItemClickListener;
+import com.example.holaorder.Model.Cart;
+import com.example.holaorder.Model.Category;
+import com.example.holaorder.Prevalent.Prevalent;
+import com.example.holaorder.ViewHolder.CartViewHolder;
+import com.example.holaorder.ViewHolder.CategoryViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class CartActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button OrderBtn;
+    private TextView txtTotalAmount;
+    DatabaseReference table_cart;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        table_cart = database.getReference("CartList")
+                .child("CartView")
+                .child(Prevalent.currentOnlineUser.getPhone())
+                .child("Foods");
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv_cart);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+
+
+        loadCart();
+
+        OrderBtn = (Button) findViewById(R.id.btn_order_C);
+        txtTotalAmount = (TextView) findViewById(R.id.tv_total_C);
+
+
+    }
+
+    private void loadCart() {
+
+        FirebaseRecyclerOptions<Cart> options =
+                new FirebaseRecyclerOptions.Builder<Cart>()
+                        .setQuery(table_cart, Cart.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter
+                = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart, parent,false);
+                CartViewHolder holder = new CartViewHolder(view);
+                return holder;
+
+            }
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, @NonNull Cart model) {
+                Picasso.get().load(model.getImage()).into(holder.imgFood);
+                holder.tvFoodName.setText(model.getFname());
+                holder.tvPrice.setText("Price: " + model.getPrice() + "$");
+//                holder.tvQuantity.setText("Quantity" + model.getQuantity());
+                Cart clickItem = model;
+                Log.d("Cart", clickItem.toString());
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(CartActivity.this, clickItem.getFname(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+}
--- HolaOrder/app/src/main/java/com/example/holaorder/Home.java
+++ HolaOrder/app/src/main/java/com/example/holaorder/Home.java
@@ -3,7 +3,10 @@ package com.example.holaorder;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,9 +30,11 @@ import com.example.holaorder.Domain.FoodDomain;
 import com.example.holaorder.Interface.ItemClickListener;
 import com.example.holaorder.Model.Category;
 import com.example.holaorder.Model.Food;
+import com.example.holaorder.Prevalent.Prevalent;
 import com.example.holaorder.ViewHolder.CategoryViewHolder;
 import com.example.holaorder.ViewHolder.FoodViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -37,12 +43,13 @@ import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
 
-
+    Button LogoutBtn;
     DatabaseReference table_category;
     DatabaseReference table_product;
     TextView textItem;
-    private RecyclerView.Adapter adapter, adapter2 ;
+    private RecyclerView.Adapter adapter, adapter2;
     private RecyclerView recyclerViewCaregoryList, recyclerViewPopularList;
+
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +61,28 @@ public class Home extends AppCompatActivity {
         table_product = database.getReference("Food");
         DatabaseReference table_product = database.getReference("Product");
 
-        ((TextView) findViewById(R.id.textHello)).setText("Hello, " + Common.currentUser.getName());
+        ((TextView) findViewById(R.id.textHello)).setText("Hello, " + Prevalent.currentOnlineUser.getPhone());
         recyclerViewCaregory();
         recyclerViewPopular();
 
-        textItem = ((TextView)findViewById(R.id.textItem));
+        textItem = ((TextView) findViewById(R.id.textItem));
 
         textItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent product = new Intent(Home.this,ListProduct.class);
+                Intent product = new Intent(Home.this, ListProduct.class);
                 startActivity(product);
+            }
+        });
+
+        LogoutBtn = (Button) findViewById(R.id.textView5);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Prevalent.currentOnlineUser = null;
+                Intent intent = new Intent(Home.this, SignIn.class);
+                startActivity(intent);
             }
         });
 
@@ -72,8 +90,8 @@ public class Home extends AppCompatActivity {
     }
 
     private void recyclerViewCaregory() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
-        recyclerViewCaregoryList=findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCaregoryList = findViewById(R.id.recyclerView);
         recyclerViewCaregoryList.setLayoutManager(linearLayoutManager);
 
         /*ArrayList<CategoryDomain> category = new ArrayList<CategoryDomain>();
@@ -86,9 +104,39 @@ public class Home extends AppCompatActivity {
         adapter = new CategoryAdapter(category);
         recyclerViewCaregoryList.setAdapter(adapter);*/
 
-        FirebaseRecyclerAdapter<Category, CategoryViewHolder> adapter = new FirebaseRecyclerAdapter<Category, CategoryViewHolder>(Category.class, R.layout.viewholder_category, CategoryViewHolder.class, table_category) {
+//        FirebaseRecyclerAdapter<Category, CategoryViewHolder> adapter = new FirebaseRecyclerAdapter<Category, CategoryViewHolder>(Category.class, R.layout.viewholder_category, CategoryViewHolder.class, table_category) {
+//            @Override
+//            protected void populateViewHolder(CategoryViewHolder categoryViewHolder, Category category, int i) {
+//                categoryViewHolder.tvCategoryName.setText(category.getName());
+//                Picasso.get().load(category.getImage()).into(categoryViewHolder.imgCategory);
+//                Category clickItem = category;
+//                Log.d("Food", category.toString());
+//                categoryViewHolder.setItemClickListener(new ItemClickListener() {
+//                    @Override
+//                    public void onClick(View view, int position, boolean isLongClick) {
+//                        Toast.makeText(Home.this, clickItem.getName(), Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                });
+//
+//            }
+//        };
+        FirebaseRecyclerOptions<Category> options =
+                new FirebaseRecyclerOptions.Builder<Category>()
+                        .setQuery(table_category, Category.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Category, CategoryViewHolder> adapter = new FirebaseRecyclerAdapter<Category, CategoryViewHolder>(options) {
+            @NonNull
             @Override
-            protected void populateViewHolder(CategoryViewHolder categoryViewHolder, Category category, int i) {
+            public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category, parent, false);
+                CategoryViewHolder holder = new CategoryViewHolder(view);
+                return holder;
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull CategoryViewHolder categoryViewHolder, int position, @NonNull Category category) {
                 categoryViewHolder.tvCategoryName.setText(category.getName());
                 Picasso.get().load(category.getImage()).into(categoryViewHolder.imgCategory);
                 Category clickItem = category;
@@ -104,17 +152,18 @@ public class Home extends AppCompatActivity {
             }
         };
         recyclerViewCaregoryList.setAdapter(adapter);
+        adapter.startListening();
     }
 
-    private void recyclerViewPopular(){
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+    private void recyclerViewPopular() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerViewPopularList = findViewById(R.id.recyclerView2);
         recyclerViewPopularList.setLayoutManager(linearLayoutManager);
 
         ArrayList<FoodDomain> foodList = new ArrayList<>();
-        foodList.add(new FoodDomain("Pepperoni Pizza", "pizza1", "slices pepperoni, mozzerella cheese",9.76));
-        foodList.add(new FoodDomain("Cheese Burger", "burger", "beef, cheese, sauce, tomato",8.79));
-        foodList.add(new FoodDomain("Vegetable pizza", "pizza2", "olive oil, vegetable oil, cherry tomatoes, basil",8.5));
+        foodList.add(new FoodDomain("Pepperoni Pizza", "pizza1", "slices pepperoni, mozzerella cheese", 9.76));
+        foodList.add(new FoodDomain("Cheese Burger", "burger", "beef, cheese, sauce, tomato", 8.79));
+        foodList.add(new FoodDomain("Vegetable pizza", "pizza2", "olive oil, vegetable oil, cherry tomatoes, basil", 8.5));
 
         adapter2 = new PopularAdapter(foodList);
         recyclerViewPopularList.setAdapter(adapter2);
