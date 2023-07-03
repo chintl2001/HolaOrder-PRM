703d2ed32cf06d0489fde1b5fd63a1a2b5f648f7
chilinhnguyen <nguyenlinhchitqp@gmail.com>
2023-07-03 09:48:22 +0700

    add menu nav bar

--- HolaOrder/app/src/main/java/com/example/holaorder/ListProduct.java
+++ HolaOrder/app/src/main/java/com/example/holaorder/ListProduct.java
@@ -1,32 +1,51 @@
 package com.example.holaorder;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Debug;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
 import com.example.holaorder.Common.Common;
 import com.example.holaorder.Interface.ItemClickListener;
 import com.example.holaorder.Model.Category;
 import com.example.holaorder.Model.Food;
+import com.example.holaorder.Model.User;
 import com.example.holaorder.ViewHolder.CategoryViewHolder;
 import com.example.holaorder.ViewHolder.FoodViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
+import com.google.firebase.storage.FirebaseStorage;
 import com.squareup.picasso.Picasso;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import java.util.Objects;
 
 public class ListProduct extends AppCompatActivity {
     RecyclerView recyclerViewCategory, recyclerViewProduct;
     DatabaseReference table_category;
     DatabaseReference table_product;
+    NavigationView navigationView;
+    CircleImageView imgUser;
+    TextView txtFullName, txtEmail;
+    ImageView imgUpload;
+    DrawerLayout drawerLayout;
+
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +57,7 @@ public class ListProduct extends AppCompatActivity {
          table_category = database.getReference("Category");
          table_product = database.getReference("Foods");
         //set current user
-        ((TextView) findViewById(R.id.tvHelloUser)).setText("Hello, " + Common.currentUser.getName());
+        //((TextView) findViewById(R.id.tvHelloUser)).setText("Hello, " + Common.currentUser.getName());
         //load category
         recyclerViewCategory = (RecyclerView) findViewById(R.id.categoryRecyclerView);
         recyclerViewCategory.setHasFixedSize(true);
@@ -49,10 +68,11 @@ public class ListProduct extends AppCompatActivity {
         recyclerViewProduct = (RecyclerView) findViewById(R.id.productRecyclerView);
         GridLayoutManager layoutManagerGrid = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         recyclerViewProduct.setLayoutManager(layoutManagerGrid);
-       ((TextView) findViewById(R.id.tvHelloUser)).setText(Common.currentUser.getName());
+       //((TextView) findViewById(R.id.tvHelloUser)).setText(Common.currentUser.getName());
 
         loadCategory();
         loadProduct();
+        NavSettup();
 
 
     }
@@ -98,4 +118,35 @@ public class ListProduct extends AppCompatActivity {
         };
         recyclerViewProduct.setAdapter(adapter);
     }
+
+    private void NavSettup() {
+        navigationView = findViewById(R.id.navbar);
+        imgUser = navigationView.getHeaderView(0).findViewById(R.id.img_user);
+        txtFullName = navigationView.getHeaderView(0).findViewById(R.id.txt_nav_name);
+        txtEmail = navigationView.getHeaderView(0).findViewById(R.id.txt_user_name);
+        imgUpload = navigationView.getHeaderView(0).findViewById(R.id.img_upload);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        User user = Common.currentUser;
+        Picasso.get().load(user.getImage()).into(imgUser);
+        txtFullName.setText(user.getName());
+        txtEmail.setText(user.getEmail());
+        //TODO: upload profile
+
+        findViewById(R.id.img_menu).setOnClickListener(v -> ShowNavigationBar());
+    }
+    private void ShowNavigationBar() {
+        findViewById(R.id.img_menu).setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.END));
+
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
