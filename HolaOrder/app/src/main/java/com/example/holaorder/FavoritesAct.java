5e095364c340048b48f8e5ce55039740faff5e1b
chilinhnguyen <nguyenlinhchitqp@gmail.com>
2023-07-17 05:01:53 +0700

    add favorites

---
+++ HolaOrder/app/src/main/java/com/example/holaorder/FavoritesAct.java
@@ -0,0 +1,13 @@
+package com.example.holaorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoritesAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
