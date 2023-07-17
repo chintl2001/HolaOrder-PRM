fc41b3d09ec8d43c4e813f4ecfc364f088b3b3ec
quynhbn272 <115777816+quynhbn27@users.noreply.github.com>
2023-07-17 13:43:35 +0700

    AddFoodDone

--- HolaOrder/app/src/main/java/com/example/holaorder/Sellers/SellerHomeActivity.java
+++ HolaOrder/app/src/main/java/com/example/holaorder/Sellers/SellerHomeActivity.java
@@ -5,21 +5,15 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.holaorder.Home;
 import com.example.holaorder.MainActivity;
 import com.example.holaorder.R;
-import com.example.holaorder.SignIn;
-import com.example.holaorder.databinding.ActivitySellerHomeBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-import io.reactivex.rxjava3.annotations.NonNull;
 
 
 public class SellerHomeActivity extends AppCompatActivity {
@@ -35,16 +29,18 @@ public class SellerHomeActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_add:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    Intent intentCategory = new Intent(SellerHomeActivity.this, SellerCategory.class);
+                    startActivity(intentCategory);
+                    finish();
                     return true;
                 case R.id.navigation_logout:
                     final FirebaseAuth mAuth;
                     mAuth = FirebaseAuth.getInstance();
                     mAuth.signOut();
 
-                    Intent intent = new Intent(SellerHomeActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
+                    Intent intentMain = new Intent(SellerHomeActivity.this, MainActivity.class);
+                    intentMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intentMain);
                     finish();
                     return true;
                 default:
